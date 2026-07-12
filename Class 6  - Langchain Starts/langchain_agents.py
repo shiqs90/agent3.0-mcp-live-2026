e9ad4c8ac8e1c4aeec8ba5c6bb1b42960608f1ec
Mayank Aggarwal <31075014+mayank953@users.noreply.github.com>
2026-07-12 10:50:31 +0530

    Langhcain

---
+++ Class 6  - Langchain Starts/langchain_agents.py
@@ -0,0 +1,21 @@
+# pip install -qU langchain "langchain[openai]"
+from dotenv import load_dotenv
+
+from langchain.agents import create_agent
+
+load_dotenv()
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    model="openai:gpt-5.5",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+result = agent.invoke(
+    {"messages": [{"role": "user", "content": "What's the weather in San Francisco?"}]}
+)
+print(result["messages"][-1].content_blocks)
